830bccb94b86f76b38ed355ef129eaef7d727c96
Bobby Curtis <bobby.curtis@oracle.com>
2019-12-19 11:28:40 -0500

    added obey file examples

--- 19c-multi-boxes-instant-client/scripts/configureNginx.sh
+++ 19c-multi-boxes-instant-client/scripts/configureNginx.sh
@@ -9,7 +9,7 @@ echo ${JAVA_HOME}
 echo ${OGG_HOME}
 
 cd ${OGG_HOME}/lib/utl/reverseproxy
-./ReverseProxySettings -u $vServiceManagerUser -p $vPassword -o ogg.conf https://localhost:$vPort
+./ReverseProxySettings -u $vServiceManagerUser -P $vPassword -o ogg.conf https://localhost:$vPort
 sudo cp ogg.conf /etc/nginx/conf.d/nginx.conf
 sudo sh /etc/ssl/certs/make-dummy-cert /etc/nginx/ogg.pem
 sudo nginx
